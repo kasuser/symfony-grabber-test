aaae65c6a39077ef503ce70c588267fb865a9233
kasuser <james.salvador.3000@gmail.com>
2020-07-05 19:16:28 +0700

    removed validation from controller

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -2,14 +2,13 @@
 
 namespace App\Controller;
 
+use Enqueue\Client\Message;
 use Enqueue\Client\ProducerInterface;
-use Psr\Log\InvalidArgumentException;
+use Ramsey\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class TagController extends AbstractController
 {
@@ -27,20 +26,19 @@ class TagController extends AbstractController
     /**
      * @Route("/tags", name="tags_post", methods={"POST"})
      */
-    public function post(Request $request, ValidatorInterface $validator, ProducerInterface $producer): Response
+    public function post(Request $request, ProducerInterface $producer): Response
     {
         $url = $request->get('url');
 
-        $violations = $validator->validate($url, [new Assert\Url()]);
+        $jobId = Uuid::uuid4()->toString();
 
-        if (count($violations) > 0) {
-            throw new InvalidArgumentException($violations->get(0)->getMessage());
-        }
+        $message = new Message('Message body', ['id' => $jobId, 'url' => $url], []);
+        $message->setMessageId($jobId);
 
-        $producer->sendEvent('grabberTopic', $url);
+        $producer->sendEvent('grabberTopic', $message);
 
-        return $this->json([
-            'message' => 'OK',
-        ]);
+        return $this->json(['data' => [
+            'job_id' => $jobId,
+        ]]);
     }
 }
