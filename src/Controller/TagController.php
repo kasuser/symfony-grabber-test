79bd09a42849104b431e0db50894736ec1fcb372
kasuser <james.salvador.3000@gmail.com>
2020-07-05 14:22:46 +0700

    added required dependencies

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -2,19 +2,42 @@
 
 namespace App\Controller;
 
+use Psr\Log\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class TagController extends AbstractController
 {
     /**
-     * @Route("/tag", name="tag", methods={"GET"})
+     * @Route("/tags/{id}", name="tags_get_by_id", methods={"GET"}, requirements={"id": "\d+"})
      */
-    public function index()
+    public function getById(int $id): Response
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/TagController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/tags", name="tags_post", methods={"POST"})
+     */
+    public function post(Request $request, ValidatorInterface $validator): Response
+    {
+        $url = $request->get('url');
+
+        $violations = $validator->validate($url, [new Assert\Url()]);
+
+        if (count($violations) > 0) {
+            throw new InvalidArgumentException($violations->get(0)->getMessage());
+        }
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
         ]);
     }
 }
